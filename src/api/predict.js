660f3e7b7740dc64febfce3ef4bc8a3917576ad9
Rajeev Singh <rsinghh68@gmail.com>
2024-12-12 14:56:57 +0530

    add search api and modify predict to include patient name

--- src/api/predict.js
+++ src/api/predict.js
@@ -1,4 +1,4 @@
-export const analyzeImage = async (image, user) => {
+export const analyzeImage = async (image, user, patientName) => {
 	try {
 	  const token = await user.getIdToken(); // Fetch Firebase token for authentication
   
@@ -6,6 +6,7 @@ export const analyzeImage = async (image, user) => {
 	  const formData = new FormData();
 	  formData.append("user_id", user.uid || "default_user_id"); // Use a unique identifier for the user
 	  formData.append("firebase_token", token); // Send the Firebase token as required by the backend
+	  formData.append("patient_name", patientName); // Attach the patient's name
 	  formData.append("image", image); // Attach the uploaded image
   
 	  const backendUrl = process.env.REACT_APP_BACKEND_URL || "http://localhost:8000";
